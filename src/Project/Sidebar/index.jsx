27b2f22a46180ea09ec940f7574d8c041fee2cdc
Nguyen Tan Khanh <khanhdpdx@gmail.com>
2022-11-23 22:16:38 +0700

    Add sidebar

---
+++ src/Project/Sidebar/index.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { NavLink, useRouteMatch } from 'react-router-dom';
+
+import { ProjectCategoryCopy } from 'constants/projects';
+import { Icon, ProjectAvatar } from 'components';
+
+import {
+  Sidebar,
+  ProjectInfo,
+  ProjectTexts,
+  ProjectName,
+  ProjectCategory,
+  Divider,
+  LinkItem,
+  LinkText,
+  NotImplemented,
+} from './Styles';
+
+const propTypes = {
+  project: PropTypes.object.isRequired,
+};
+
+const ProjectSidebar = ({ project }) => {
+  const match = useRouteMatch();
+
+  return (
+    <Sidebar>
+      <ProjectInfo>
+        <ProjectAvatar />
+        <ProjectTexts>
+          <ProjectName>{project.name}</ProjectName>
+          <ProjectCategory>{ProjectCategoryCopy[project.category]} project</ProjectCategory>
+        </ProjectTexts>
+      </ProjectInfo>
+
+      {renderLinkItem(match, 'Board', 'board', '/board')}
+      {renderLinkItem(match, 'Project settings', 'settings', '/settings')}
+      <Divider />
+      {/* {renderLinkItem(match, 'Releases', 'shipping')}
+      {renderLinkItem(match, 'Issues and filters', 'issues')}
+      {renderLinkItem(match, 'Pages', 'page')}
+      {renderLinkItem(match, 'Reports', 'reports')}
+      {renderLinkItem(match, 'Components', 'component')} */}
+      {renderLinkItem(match, 'Project create', 'board', '/project-create')}
+      {renderLinkItem(match, 'Backlog', 'page', '/backlog')}
+      {renderLinkItem(match, 'Grantt', 'shipping', '/grantt')}
+      {renderLinkItem(match, 'Burndown chart', 'calendar', '/burndown-chart')}
+    </Sidebar>
+  );
+};
+
+const renderLinkItem = (match, text, iconType, path) => {
+  const isImplemented = !!path;
+
+  const linkItemProps = isImplemented
+    ? { as: NavLink, exact: true, to: `${match.path}${path}` }
+    : { as: 'div' };
+
+  return (
+    <LinkItem {...linkItemProps}>
+      <Icon type={iconType} />
+      <LinkText>{text}</LinkText>
+      {!isImplemented && <NotImplemented>Not implemented</NotImplemented>}
+    </LinkItem>
+  );
+};
+
+ProjectSidebar.propTypes = propTypes;
+
+export default ProjectSidebar;

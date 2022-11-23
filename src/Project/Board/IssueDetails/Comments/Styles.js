3b4e36d4b319b90a6fad5851d5d5a043b757da4d
Nguyen Tan Khanh <khanhdpdx@gmail.com>
2022-11-23 22:22:14 +0700

    Add scrum board

---
+++ src/Project/Board/IssueDetails/Comments/Styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+import { font } from 'react-project-management';
+
+export const Comments = styled.div`
+  padding-top: 40px;
+`;
+
+export const Title = styled.div`
+  ${font.medium}
+  ${font.size(15)}
+`;
